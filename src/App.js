821a75c05029768f36ba91b0d222ef4f3c12539b
beka-bat1 <bekabatmanashvili1@gmail.com>
2021-04-04 16:23:00 +0400

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar'
+import Particles from "react-particles-js";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+    <Particles 
+    className="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 6,
+            color: '#f9ab00'
+          }
+        }
+      }
+    }} />
     <Navbar />
-      <h1>Portfolio Project...</h1>
+     <Header />
     </div>
   );
 }
